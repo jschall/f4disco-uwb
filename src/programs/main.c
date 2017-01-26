40c81ae243a864acb64a4a3047849529285a8195
Jonathan Challinger <mr.challinger@gmail.com>
2017-01-26 15:55:37 -0800

    clock setup

--- src/programs/main.c
+++ src/programs/main.c
@@ -1,4 +1,10 @@
-void main(void)
-{
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
+int main(void)
+{
+    rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+
+
+    return 0;
 }
